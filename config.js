bbeb2cf0f1b377ef257c645bd2cbf2295a4bcc35
Laylead <161890363+Laylead@users.noreply.github.com>
2025-06-01 01:03:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 // config.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyBlh428ba5aSFt6x2pPkVjeEk5D6AbIU7Y",
-  authDomain: "kheemz-fb9b6.firebaseapp.com",
-  projectId: "kheemz-fb9b6",
-  storageBucket: "kheemz-fb9b6.firebasestorage.app",
-  messagingSenderId: "593310501630",
-  appId: "1:593310501630:web:c241a05e0d7c4a810173af",
-  measurementId: "G-Q50WK6G76P"
+const firebaseConfig = {
+    apiKey: "AIzaSyBlh428ba5aSFt6x2pPkVjeEk5D6AbIU7Y",
+    authDomain: "kheemz-fb9b6.firebaseapp.com",
+    databaseURL: "https://kheemz-fb9b6-default-rtdb.firebaseio.com",
+    projectId: "kheemz-fb9b6",
+    storageBucket: "kheemz-fb9b6.appspot.com",
+    messagingSenderId: "593310501630",
+    appId: "1:593310501630:web:c241a05e0d7c4a810173af",
+    measurementId: "G-Q50WK6G76P"
 };
-
-export const agoraAppId = "974f4c7b124940a1a6ee5e526175118d";
